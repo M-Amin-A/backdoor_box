8627e9598a3c4a4216ef6f859c6dfeb58bf7f9b8
M.Amin <m.a.abbasfar@gmail.com>
2024-08-28 14:18:19 +0330

    update

---
+++ custom.py
@@ -0,0 +1,89 @@
+import os
+
+import cv2
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset
+import torchvision
+from torchvision.transforms import Compose, ToTensor, PILToTensor, RandomHorizontalFlip
+
+import core
+
+
+dataset = torchvision.datasets.DatasetFolder
+
+# image file -> cv.imread -> numpy.ndarray (H x W x C) -> ToTensor -> torch.Tensor (C x H x W) -> RandomHorizontalFlip -> torch.Tensor -> network input
+transform_train = Compose([
+    ToTensor(),
+    RandomHorizontalFlip()
+])
+trainset = dataset(
+    root='./data/cifar10/train',
+    loader=cv2.imread,
+    extensions=('png',),
+    transform=transform_train,
+    target_transform=None,
+    is_valid_file=None)
+
+transform_test = Compose([
+    ToTensor()
+])
+testset = dataset(
+    root='./data/cifar10/test',
+    loader=cv2.imread,
+    extensions=('png',),
+    transform=transform_train,
+    target_transform=None,
+    is_valid_file=None)
+
+
+pattern = torch.zeros((1, 32, 32), dtype=torch.uint8)
+pattern[0, -3:, -3:] = 255
+weight = torch.zeros((1, 32, 32), dtype=torch.float32)
+weight[0, -3:, -3:] = 1.0
+
+badnets = core.BadNets(
+    train_dataset=trainset,
+    test_dataset=testset,
+    model=core.models.ResNet(18),
+    # model=core.models.BaselineMNISTNetwork(),
+    loss=nn.CrossEntropyLoss(),
+    y_target=1,
+    poisoned_rate=0.05,
+    pattern=pattern,
+    weight=weight,
+    # poisoned_transform_index=0,
+    poisoned_target_transform_index=0,
+    schedule=None,
+    seed=666
+)
+
+poisoned_train_dataset, poisoned_test_dataset = badnets.get_poisoned_dataset()
+
+# train attacked model
+schedule = {
+    'device': 'GPU',
+    'CUDA_VISIBLE_DEVICES': '0',
+    'GPU_num': 1,
+
+    'benign_training': False,
+    'batch_size': 128,
+    'num_workers': 16,
+
+    'lr': 0.1,
+    'momentum': 0.9,
+    'weight_decay': 5e-4,
+    'gamma': 0.1,
+    'schedule': [150, 180],
+
+    'epochs': 200,
+
+    'log_iteration_interval': 100,
+    'test_epoch_interval': 10,
+    'save_epoch_interval': 10,
+
+    'save_dir': 'experiments',
+    'experiment_name': 'train_poisoned_DatasetFolder-CIFAR10'
+}
+
+badnets.train(schedule)
