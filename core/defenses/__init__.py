17a52a5b4b21a5ea194b3e4d1ac82a2a41534f9c
M.Amin <m.a.abbasfar@gmail.com>
2024-08-28 12:59:33 +0330

    init

---
+++ core/defenses/__init__.py
@@ -0,0 +1,14 @@
+from .ABL import ABL
+from .AutoEncoderDefense import AutoEncoderDefense
+from .ShrinkPad import ShrinkPad
+from .MCR import MCR
+from .FineTuning import FineTuning
+from .NAD import NAD
+from .Pruning import Pruning
+from .CutMix import CutMix
+from .IBD_PSC import IBD_PSC
+from .SCALE_UP import SCALE_UP
+
+__all__ = [
+    'AutoEncoderDefense', 'ShrinkPad', 'FineTuning', 'MCR', 'NAD', 'Pruning', 'ABL', 'CutMix', 'IBD_PSC', 'SCALE_UP'
+]
