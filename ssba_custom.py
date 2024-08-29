56696e8a5af8a3855008cc8904637b0b20fd888b
M.Amin <m.a.abbasfar@gmail.com>
2024-08-29 15:56:33 +0330

    update

--- ssba_custom.py
+++ ssba_custom.py
@@ -36,7 +36,7 @@ encoder_schedule = {
 
 train_steg_set = []
 for image, label in trainset:
-    secret = np.random.binomial(1, .5, secret_size)
+    secret = np.random.binomial(1, .5, secret_size).astype(np.uint8)
     train_steg_set.append([image, secret])
 
 poison_class = core.ISSBA(
