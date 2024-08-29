76bb78956a30542784fa659d716343f9104734e2
M.Amin <m.a.abbasfar@gmail.com>
2024-08-29 15:52:27 +0330

    update

--- ssba_custom.py
+++ ssba_custom.py
@@ -7,7 +7,7 @@ from torch.utils.data import Dataset
 import torchvision
 from torchvision.transforms import Compose, ToTensor, PILToTensor, RandomHorizontalFlip
 import torchvision.transforms as transforms
-
+import numpy as np
 import core
 
 transform_train = Compose([
